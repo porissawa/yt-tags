ede3033ec571b39acc8f7a39d3c66d43128327d8
Pedro Morissawa <pmorissawa@gmail.com>
2020-10-04 23:25:16 -0300

    refactor: regex on parser

--- index.js
+++ index.js
@@ -22,20 +22,19 @@ const fetchChannelVideoIdList = async channelUrl => {
 }
 
 const parseVideoMetadata = ({data}) => {
-  const channelOwner = data.match(/"ownerChannelName":\"(.*?)\"/)[0].slice(19).replace(/\"/g, '')
-  const videoTitle = data.match(/\<title\>(.*?)\<\/title\>/)[0].slice(7, -18).replace(/\"/g, '')
-
-  let keywords = JSON.stringify([])
-  const keywordsRegexMatch = data.match(/keywords\":\[(.*?)\]/)
-
-  if (keywordsRegexMatch){
-    keywords = keywordsRegexMatch[0].slice(10)
-  }
+  const videoDetails = data.match(/"videoDetails":(.*?)\"isPrivate\"/)[0].slice(0, -12)
+  const videoId = videoDetails.match(/videoId":"(.*?)"/)[1]
+  const videoTitle = videoDetails.match(/"title":"(.*?)"/)[1]
+  const keywords = videoDetails.match(/"keywords":\[(.*?)\]/)[1]
+  const channelId = videoDetails.match(/"channelId":"(.*?)"/)[1]
+  const channelOwner = videoDetails.match(/"author":"(.*?)"/)[1]
   
   return {
+    channelId,
+    owner: he.decode(channelOwner),
+    tags: keywords.split(','),
     title: he.decode(videoTitle),
-    tags: JSON.parse(keywords),
-    owner: he.decode(channelOwner)
+    videoId,
   }
 }
 
@@ -43,4 +42,12 @@ const parseVideoIdList = ({data}) => {
   return new Set(data.match(/videoIds\":\[(.*?)\]/g).map(el => el.slice(12, -2)))
 }
 
-fetchChannelVideoIdList('https://www.youtube.com/channel/UC0fGGprihDIlQ3ykWvcb9hg/videos').then(console.log)
+function runWithTime() {
+  console.time("Ran in")
+  fetchChannelVideoIdList('https://www.youtube.com/channel/UCpbiwmdeWqT7bE0_3qLuTdA/videos').then(data => {
+    console.log(data)
+    console.timeEnd("Ran in")
+  })
+}
+
+runWithTime()
